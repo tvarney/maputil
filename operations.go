cbaeba946d9701a1dfa15474f840a2e6d6809526
tvarney <troy.a.varney@gmail.com>
2021-02-28 15:09:39 -0600

    Add Copy and CopyArray functions
    The Copy and CopyArray functions provide a way to deep copy a JSON map;
    values in such a map which are not one of the JSON types are not
    deep-copied. This should be faster than a 'proper' deep-copy, where
    reflection is used to recurse into arbitrary pointer types, and should
    be sufficient for working with JSON-like data.

--- operations.go
+++ operations.go
@@ -11,3 +11,50 @@ func Keys(m map[string]interface{}) []string {
 	}
 	return keys
 }
+
+// Copy makes a deep copy of the given map.
+//
+// Only data structures which are JSON-like are handled by this function; a
+// struct pointer in a map will not be deep-copied.
+func Copy(m map[string]interface{}) map[string]interface{} {
+	if len(m) == 0 {
+		if m == nil {
+			return nil
+		}
+		return map[string]interface{}{}
+	}
+	r := make(map[string]interface{}, len(m))
+	for k, v := range m {
+		switch d := v.(type) {
+		case map[string]interface{}:
+			r[k] = Copy(d)
+		case []interface{}:
+			r[k] = CopyArray(d)
+		default:
+			r[k] = d
+		}
+	}
+	return r
+}
+
+// CopyArray makes a deep copy of the given array.
+//
+// Only data structures which are JSON-like are handled by this function; a
+// struct pointer in the array will not be deep-copied.
+func CopyArray(a []interface{}) []interface{} {
+	if len(a) == 0 {
+		return nil
+	}
+	r := make([]interface{}, 0, len(a))
+	for _, v := range a {
+		switch d := v.(type) {
+		case map[string]interface{}:
+			r = append(r, Copy(d))
+		case []interface{}:
+			r = append(r, CopyArray(d))
+		default:
+			r = append(r, d)
+		}
+	}
+	return r
+}
--- operations_test.go
+++ operations_test.go
@@ -27,3 +27,72 @@ func TestKeys(t *testing.T) {
 		require.ElementsMatch(t, keys, []string{"one", "two", "three"})
 	})
 }
+
+func TestCopy(t *testing.T) {
+	t.Parallel()
+	t.Run("Nil", func(t *testing.T) {
+		t.Parallel()
+		require.Nil(t, maputil.Copy(nil))
+	})
+	t.Run("Empty", func(t *testing.T) {
+		t.Parallel()
+		d := map[string]interface{}{}
+		c := maputil.Copy(d)
+		require.NotNil(t, c)
+		require.Equal(t, d, c)
+		// Ensure they are different instances
+		c["hello"] = "world"
+		require.NotEqual(t, d, c)
+	})
+	t.Run("NonEmpty", func(t *testing.T) {
+		t.Parallel()
+		d := map[string]interface{}{
+			"one": "two",
+			"two": []interface{}{1, 2, 3},
+			"three": map[string]interface{}{
+				"hello": "world",
+			},
+		}
+		c := maputil.Copy(d)
+		require.Equal(t, d, c)
+		// Check that the returned map is a different instance.
+		c["four"] = "test"
+		require.NotEqual(t, d, c)
+		// Check that the child array is a different instance
+		c["two"].([]interface{})[1] = 0
+		require.NotEqual(t, d["two"], c["two"])
+		// Check that the child map is a different instance
+		c["three"].(map[string]interface{})["hello"] = "test"
+		require.NotEqual(t, d["three"], c["three"])
+	})
+}
+
+func TestCopyArray(t *testing.T) {
+	t.Parallel()
+	t.Run("Nil", func(t *testing.T) {
+		t.Parallel()
+		require.Nil(t, maputil.CopyArray(nil))
+	})
+	t.Run("Empty", func(t *testing.T) {
+		t.Parallel()
+		d := []interface{}{}
+		require.Nil(t, maputil.CopyArray(d))
+	})
+	t.Run("NotEmpty", func(t *testing.T) {
+		t.Parallel()
+		a := []interface{}{1, []interface{}{1, 2, 3}, map[string]interface{}{
+			"hello": "world",
+		}}
+		c := maputil.CopyArray(a)
+		require.Equal(t, a, c)
+		// Check that the returned array is a different instance.
+		c[0] = "test"
+		require.NotEqual(t, a, c)
+		// Check that the child array is a different instance.
+		(c[1].([]interface{}))[0] = 0
+		require.NotEqual(t, a[1], c[1])
+		// Check that the child map is a different instance.
+		(c[2].(map[string]interface{}))["hello"] = "test"
+		require.NotEqual(t, a[2], c[2])
+	})
+}
